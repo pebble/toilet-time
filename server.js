c86d22374e9ac76de534480765c4a190676e6e2d
Lance R. Vick <lance@lrvick.net>
2015-05-15 18:51:04 -0700

    initial arduino-receive/pin-update server

--- server.js
+++ server.js
@@ -1,13 +1,58 @@
-var express = require('express');
-var bodyParser = require("body-parser");
-var app = express();
+var router = require('koa-joi-router');
+var koa = require('koa');
+var Timeline = require('pebble-api');
+var timeline = new Timeline();
+var token = '---------------------'; 
+var app = module.exports = koa();
+var router = router();
 
-app.use(bodyParser());
+app.use(router.middleware());
+app.listen(8080, function () {
+  console.log(arguments);
+});
 
-app.post('/v1/update', function(req, res) {
-  console.log(req.body)
-})
+router.route({
+  method: 'post',
+  path: '/v1/update',
+  validate: {
+    type: 'json'
+  },
+  handler: [
+    newStatus
+  ]
+});
 
-app.listen(8080,function(){
-  console.log("Started on PORT 8080");
-})
+function* newStatus() {
+  if (!this.request.body) {
+    this.status = 500;
+    this.body = 'You didn\'t send anything';
+    return;
+  }
+  var body = this.request.body;
+
+  yield new Promise(function(resolve, reject) {
+
+    var pin = new Timeline.Pin({
+      id: 'toilet-time',
+      time: new Date(),
+      duration: 10,
+      layout: new Timeline.Pin.Layout({
+        type: Timeline.Pin.LayoutType.GENERIC_PIN,
+        tinyIcon: Timeline.Pin.Icon.NOTIFICATION_FLAG,
+        title: 'Bathroom Status'
+      })
+    });
+    
+    timeline.sendUserPin(token, pin, function (err) {
+         if (err) {
+           reject(err);
+         }
+        resolve();
+      console.log('Pin sent successfully!');
+    });
+
+  });
+
+  console.log('%j', this.request.body);
+}
+
